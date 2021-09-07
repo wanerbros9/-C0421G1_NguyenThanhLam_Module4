97076ba1a21323bc94a4fd8a620dc027eae0566a
Nguyễn Thanh Lâm <49389939+wanerbros9@users.noreply.github.com>
2021-09-07 09:54:33 +0700

    initial commit

---
+++ 09_aop/thucthanh/bai1/src/main/java/grandemonstration/repository/ProvinceRepository.java
@@ -0,0 +1,9 @@
+package grandemonstration.repository;
+
+import grandemonstration.model.Province;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProvinceRepository extends CrudRepository<Province, Long> {
+}
