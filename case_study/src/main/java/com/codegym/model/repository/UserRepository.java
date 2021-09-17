8b1fb1cee06e9176c80a4ce9ed82384770703c11
Nguyễn Thanh Lâm <49389939+wanerbros9@users.noreply.github.com>
2021-09-17 16:20:13 +0700

    initial commit

--- case_study/src/main/java/com/codegym/model/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codegym.model.repository;
-
-import com.codegym.model.entity.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String name);
-}
